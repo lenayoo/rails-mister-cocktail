eca40e8e5b27844cefb28509f5a6e1d89ea995e2
lena yoo <lenayoo17@gmail.com>
2021-02-04 17:56:54 +0900

    was very hard

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,36 @@
 class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    @ingredient = Ingredient.new
+  end
+
+  def new
+    @cocktail = Cocktail.new(params[:cocktail])
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.destroy
+  end
+  
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
