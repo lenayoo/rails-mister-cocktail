3d66e2cd66efb24324ad96c80dc0b052b325bc35
lena yoo <lenayoo17@gmail.com>
2021-02-06 08:31:25 +0900

    done green

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "tomato juice")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "soju")
+Cocktail.create(name: "mojito")
+Cocktail.create(name: "bloody mary")
+Cocktail.create(name: "margarita")
+Dose.create(description: "6 leaves", cocktail_id: 1, ingredient_id: 3)
+Dose.create(description: "a little vodka", cocktail_id: 1, ingredient_id: 6)
+Dose.create(description: "some ice", cocktail_id: 1, ingredient_id: 2)
+Dose.create(description: "some tomato juice", cocktail_id: 2, ingredient_id: 4)
+Dose.create(description: "add some vodka", cocktail_id: 2, ingredient_id: 6)
+Dose.create(description: "add some tequila", cocktail_id: 3, ingredient_id: 5)
+Dose.create(description: "add some ice", cocktail_id: 3, ingredient_id: 2)
